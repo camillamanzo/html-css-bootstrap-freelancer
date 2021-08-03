792fb2c0f6cd0631e87e3fde64f0703987522197
camillamanzo <camillasouthafrica@gmail.com>
2021-08-03 16:19:04 +0200

    js trial run

---
+++ JavaScript/myscript.js
@@ -0,0 +1,6 @@
+
+console.log("sostituito all'elemento con id -startText- il contenuto");
+document.getElementById("startText").innerHTML = "best of bootstrap";
+
+console.log();
+document.getElementsById("portfolioImg").innerHTML = "Images";
